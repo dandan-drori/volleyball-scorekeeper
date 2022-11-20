d7622b051689b81093a89c829709297194b04826
dandan <dandan.drori@zoominfo.com>
2022-11-20 19:58:51 +0200

    fix: Start Now from home page navigates to select-teams route

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,7 +6,7 @@ function Home() {
         <div className="home">
             <div className="app-name" />
             <div className="actions">
-                <Link to="/register">Start Now</Link>
+                <Link to="/select-teams">Start Now</Link>
                 <Link to="/settings">Settings</Link>
             </div>
         </div>
